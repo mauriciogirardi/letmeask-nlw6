9eab9872ecaebd2f66ac50d90db7996567185545
mauricio girardi <mauriciogirardi@denox.com.br>
2021-06-24 00:14:51 -0300

    start page room

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,7 +1,102 @@
+import { useParams } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import { useCallback, useState, FormEvent, ChangeEvent } from 'react';
+
+import logoImg from 'assets/images/logo.svg';
+import Button from 'components/Button';
+import { database } from 'services/firebase';
+import RoomCode from 'components/RoomCode';
+import { useAuth } from 'hooks/auth';
+import * as S from './styles';
+
+type RoomParams = {
+  id: string;
+};
+
 export default function Room() {
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const [newQuestion, setNewQuestion] = useState('');
+
+  const handelSendQuestion = useCallback(
+    async (event: FormEvent) => {
+      event.preventDefault();
+
+      if (!newQuestion.trim()) {
+        return toast.error('Você deve fazer a pergunta antes de enviar!');
+      }
+
+      if (!user) {
+        return toast.error('Você precisa logar para enviar sua pergunta!');
+      }
+
+      const question = {
+        content: newQuestion,
+        author: {
+          name: user.name,
+          avatar: user.avatar,
+        },
+        isHighLighted: false,
+        isAnswered: false,
+      };
+
+      await database.ref(`rooms/${roomId}/questions`).push(question);
+
+      toast.success('Pergunta enviada!');
+      setNewQuestion('');
+    },
+    [newQuestion, user, roomId],
+  );
+
+  const onChange = useCallback((event: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = event.target;
+    setNewQuestion(value);
+  }, []);
+
   return (
-    <>
-      <h1>Room</h1>
-    </>
+    <S.Container>
+      <Toaster position="top-right" />
+
+      <S.Header>
+        <S.Content>
+          <img src={logoImg} alt="Logo Letmeask" />
+          <RoomCode code={roomId} />
+        </S.Content>
+      </S.Header>
+
+      <S.Main>
+        <S.TitleRoom>
+          <h1>Sala React</h1>
+          <span>4 perguntas</span>
+        </S.TitleRoom>
+
+        <S.Form onSubmit={handelSendQuestion}>
+          <textarea
+            placeholder="O que você quer perguntar?"
+            onChange={e => onChange(e)}
+            value={newQuestion}
+          />
+
+          <S.FooterForm>
+            {user ? (
+              <S.UserInfo>
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </S.UserInfo>
+            ) : (
+              <span>
+                Para enviar uma pergunta,
+                <button type="button">faça seu login</button>
+              </span>
+            )}
+            <Button type="submit" disabled={!user}>
+              Enviar pergunta
+            </Button>
+          </S.FooterForm>
+        </S.Form>
+      </S.Main>
+    </S.Container>
   );
 }
---
+++ src/pages/Room/styles.ts
@@ -0,0 +1,113 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const Header = styled.header`
+  padding: 1rem 2.5rem;
+  border-bottom: 1px solid #e2e2e2;
+`;
+export const Content = styled.div`
+  max-width: 1120px;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > img {
+    max-height: 2.7rem;
+  }
+`;
+
+export const Main = styled.main`
+  max-width: 800px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+`;
+
+export const TitleRoom = styled.div`
+  margin: 2rem 0 1.5rem 0;
+  display: flex;
+  align-items: center;
+
+  h1 {
+    font-family: Poppins, sans-serif;
+    font-size: 1.5rem;
+  }
+
+  span {
+    margin-left: 1rem;
+    background-color: var(--pink-dark);
+    padding: 0.5rem 1rem;
+    color: var(--details);
+    border-radius: 9999px;
+    font-size: 0.9rem;
+  }
+`;
+
+export const Form = styled.form`
+  textarea {
+    width: 100%;
+    border: 0;
+    padding: 1rem;
+    background-color: var(--details);
+    border-radius: 0.5rem;
+    box-shadow: 0 2px 12px rgba(0, 0, 0, 0.04);
+    resize: vertical;
+    height: 9rem;
+    outline-color: var(--gray-light);
+  }
+`;
+export const UserInfo = styled.div`
+  display: flex;
+  align-items: center;
+
+  span {
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+
+  > img {
+    border-radius: 50%;
+    width: 32px;
+    height: 32px;
+    margin-right: 0.5rem;
+  }
+`;
+
+export const FooterForm = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1rem;
+
+  > span {
+    font-size: 0.9rem;
+    font-weight: 500;
+    color: var(--gray-dark);
+
+    button {
+      background-color: transparent;
+      border: 0;
+      color: var(--purple);
+      text-decoration: underline;
+      font-size: 0.9rem;
+      font-weight: 500;
+      margin-left: 0.3rem;
+    }
+  }
+
+  > button {
+    width: 200px;
+  }
+
+  @media (max-width: 500px) {
+    > button {
+      width: 130px;
+    }
+
+    > span {
+      margin-top: 1rem;
+    }
+  }
+`;
